34a54d03c5d52c3c4f696c84612e9ee966bbefae
kmf5285 <kmf5285@rit.edu>
2018-09-12 19:04:34 -0400

    splitting tests

--- unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
+++ unittests/edu/ncsu/csc326/coffeemaker/InventoryTest.java
@@ -29,21 +29,25 @@ public class InventoryTest extends TestCase {
 		super.setUp();
 	}
 	
-	public void testSetChocolate() {
-        i1.setChocolate(0);
+	public void testSetChocolateLTZero() {
+        i1.setChocolate(-1);
+        chocolate = i1.getChocolate();
+        assertEquals(15,chocolate);
+	}
+	
+	public void testSetChocolateZero() {
+		i1.setChocolate(0);
         chocolate = i1.getChocolate();
         assertEquals(0,chocolate);
-
-        i1.setChocolate(1);
-        chocolate = i1.getChocolate();
-        assertEquals(1,chocolate);
-
-        i1.setChocolate(-1);
+	}
+	
+	public void testSetChocolateGTZero() {
+		i1.setChocolate(1);
         chocolate = i1.getChocolate();
         assertEquals(1,chocolate);
 	}
 	
-	public void testAddChocolate() {
+	public void testAddChocolateInvalid() {
 		// Test invalid string input. Should throw InventoryException.
         try{
             i1.addChocolate("seven");
@@ -54,8 +58,10 @@ public class InventoryTest extends TestCase {
             assertNotSame("Input for chocolate must be a positive int", e);
             assertTrue("Input for chocolate must be a positive int", chocolate >= 0);
         }
-
-        // Test negative value input. Should throw InventoryException.
+	}
+	
+	public void testAddChocolateLTZero() {
+		// Test negative value input. Should throw InventoryException.
         try{
 	        i1.addChocolate("-1");
 	        chocolate = i1.getChocolate();
@@ -64,7 +70,9 @@ public class InventoryTest extends TestCase {
             //assertNotEquals("Units of chocolate must be a positive integer",e);
             assertTrue("Input for coffee must be a positive int", chocolate > 0);
         }
-
+	}
+	
+	public void testAddChocolateGTZero() {
         // Test valid input
         try {
 			i1.addChocolate("2");
@@ -76,24 +84,28 @@ public class InventoryTest extends TestCase {
         assertEquals(17, chocolate);
 	}
 	
-	public void testSetCoffee() {
+	public void testSetCoffeeZero() {
 		// Test setting to 0
         i1.setCoffee(0);
         coffee = i1.getCoffee();
         assertEquals(0, coffee);
-
-        // Test input > 0
+	}
+	
+	public void testSetCoffeeGTZero() {
+		// Test input > 0
         i1.setCoffee(1);
-        coffee = i1.getCoffee();
-        assertEquals(1, coffee);
-
-        // Test input < 0
-        i1.setChocolate(-1);
         coffee = i1.getCoffee();
         assertEquals(1, coffee);
 	}
 	
-	public void testAddCoffee() {
+	public void testSetCoffeeLTZero() {
+        // Test input < 0
+        i1.setChocolate(-1);
+        coffee = i1.getCoffee();
+        assertEquals(15, coffee);
+	}
+	
+	public void testAddCoffeeInvalid() {
 		// Test invalid string input. Should throw InventoryException.
         try{
             i1.addCoffee("two");
@@ -104,9 +116,10 @@ public class InventoryTest extends TestCase {
             assertNotSame("Input for chocolate must be a positive int", e);
             assertTrue("Input for coffee must be a positive int", coffee >=0);
         }
-
-
-        // Test input < 0
+	}
+	
+	public void testAddCoffeeLTZero() {
+		// Test input < 0
         try{
             i1.addCoffee("-1");
             coffee = i1.getCoffee();
@@ -114,37 +127,44 @@ public class InventoryTest extends TestCase {
         catch(InventoryException e){
             assertTrue("Units of coffee must be a positive integer", coffee > 0);
         }
-
-
+	}
+	
+	public void testAddCoffee() {
         // Test valid input
         try {
 			i1.addCoffee("2");
 		} catch (InventoryException e) {
 			fail("InventoryException should not be thrown");
 		}
-        
-        coffee = i1.getCoffee();
-        assertEquals(17, coffee);
 	}
 	
-	public void testSetMilk() {
+	public void testGetCoffee() {
+		coffee = i1.getCoffee();
+        assertEquals(15, coffee);
+	}
+	
+	public void testSetMilkZero() {
 		// Test input = 0
         i1.setMilk(0);
         milk = i1.getMilk();
         assertEquals(0,milk);
-
-        // Test input > 0
+	}
+	
+	public void testSetMilkGTZero() {
+		// Test input > 0
         i1.setMilk(1);
-        milk = i1.getMilk();
-        assertEquals(1,milk);
-
-        // Test input < 0
-        i1.setMilk(-1);
         milk = i1.getMilk();
         assertEquals(1,milk);
 	}
 	
-	public void testAddMilk() {
+	public void testSetMilkLTZero() {
+        // Test input < 0
+        i1.setMilk(-1);
+        milk = i1.getMilk();
+        assertEquals(15,milk);
+	}
+	
+	public void testAddMilkInvalid() {
 		// Test invalid string input. Should throw InventoryException.
         try{
             i1.addMilk("two");
@@ -155,9 +175,10 @@ public class InventoryTest extends TestCase {
             assertNotSame("Input for milk must be a positive int",e);
             assertTrue("Input for milk must be a positive int", milk >= 0);
         }
-
-
-        // Test input < 0
+	}
+	
+	public void testAddMilkLTZero() {
+		// Test input < 0
         try{
             i1.addMilk("-1");
             milk = i1.getMilk();
@@ -165,8 +186,9 @@ public class InventoryTest extends TestCase {
         catch(InventoryException e){
             assertTrue("Input for milk must be a positive int", milk > 0);
         }
-
-
+	}
+	
+	public void testAddMilk() {
         // Test valid input
         try {
 			i1.addMilk("2");
@@ -177,24 +199,28 @@ public class InventoryTest extends TestCase {
         assertEquals(17,milk);
 	}
 	
-	public void testSetSugar() {
+	public void testSetSugarZero() {
 		// Test input = 0
         i1.setSugar(0);
         sugar = i1.getSugar();
         assertEquals(0,sugar);
-
-        // Test input > 0
+	}
+	
+	public void testSetSugarGTZero() {
+		// Test input > 0
         i1.setSugar(1);
-        sugar = i1.getSugar();
-        assertEquals(1,sugar);
-
-        // Test input < 0
-        i1.setSugar(-1);
         sugar = i1.getSugar();
         assertEquals(1,sugar);
 	}
 	
-	public void testAddSugar() {
+	public void testSetSugarLTZero() {
+        // Test input < 0
+        i1.setSugar(-1);
+        sugar = i1.getSugar();
+        assertEquals(15,sugar);
+	}
+	
+	public void testAddSugarInvalid() {
 		// Test invalid string input. Should throw InventoryException
         try{
             i1.addSugar("two");
@@ -203,9 +229,10 @@ public class InventoryTest extends TestCase {
         catch(InventoryException e){
             assertNotSame("Input for sugar ", e);
         }
-
-
-        //Check for the equality operator for the lower boundary input value = -1. Expected to receive an exception message.
+	}
+	
+	public void testAddSugarLTZero() {
+		//Check for the equality operator for the lower boundary input value = -1. Expected to receive an exception message.
         try{
             i1.addSugar("-1");
             sugar = i1.getSugar();
@@ -213,8 +240,9 @@ public class InventoryTest extends TestCase {
         catch(InventoryException e){
             assertTrue("Input for sugar must be a positive int", sugar > 0);
         }
-
-
+	}
+	
+	public void testAddSugar() {
         // Test valid input
         try {
 			i1.addSugar("2");
@@ -426,3 +454,4 @@ public class InventoryTest extends TestCase {
         }
 	}
 }
+
